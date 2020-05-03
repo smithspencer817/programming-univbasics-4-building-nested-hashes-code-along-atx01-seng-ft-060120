f8c2c05e11d92dfc7c0afd0feca5f547d17e108c
Spencer Smith <smithspencer817@gmail.com>
2020-05-03 22:41:26 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,11 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }
+  }
 
   
 end
